a22fa3db672e9e11a1b0892372045a7919c1c2f9
Adam Balogh <baloghbh@gmail.com>
2015-06-29 14:30:01 +0000

    Remove unused path

--- paths.php
+++ paths.php
@@ -7,6 +7,5 @@ return [
     'routes'                        => __DIR__ . '/app/routes.php',
     'services'                      => __DIR__ . '/app/services.php',
     'config.logger'                 => __DIR__ . '/app/config/' . $env . 'logger.php',
-    'config.service.orm'            => __DIR__ . '/app/config/' . $env . 'orm.php',
-    'config.api_version_handler'    => __DIR__ . '/app/config/' . $env . 'api_version_handler.php',
+    'config.service.orm'            => __DIR__ . '/app/config/' . $env . 'orm.php'
 ];
